6fb99588d1cc0071deacba9afe33c77d2eb09d35
Falko Riemenschneider <riemenschneider@doctronic.de>
2015-10-14 11:35:38 +0200

    Dropped 1.6 support

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/friemen/examine"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371" :scope "provided"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.48" :scope "provided"]]
   :jar-exclusions [#"\.cljx"]
   :plugins [[codox "0.8.10"]
             [org.clojars.frozenlock/cljx "0.4.6"]
-            [lein-cljsbuild "1.0.4-SNAPSHOT"]
+            [lein-cljsbuild "1.1.0"]
             [com.cemerick/clojurescript.test "0.3.1"]]
   :codox {:defaults {}
           :sources ["src/clj" "target/classes"]
@@ -20,20 +20,19 @@
         :url "https://github.com/friemen/examine"}
   :repositories [["clojars" {:url "https://clojars.org/repo"
                              :creds :gpg}]]
-  :hooks [cljx.hooks]
+;  :hooks [cljx.hooks]
   :source-paths ["src/clj"]
-  :test-paths ["target/test-classes"]
-  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.5"]]
+  :test-paths ["target/clj" "target/clj-test"]
+  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.6"]]
                    :injections [(require 'cemerick.austin.repls)
                                 (defn browser-repl []
                                   (cemerick.austin.repls/cljs-repl (reset! cemerick.austin.repls/browser-repl-env
                                                                            (cemerick.austin/repl-env))))]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
 
   :cljsbuild {:test-commands {"unit-tests" ["phantomjs" :runner "target/testable.js"]}
-              :builds [{:source-paths ["src/clj" "target/classes" "target/test-classes"]
+              :builds [{:source-paths ["src/clj" "target/clj" "target/cljs" "target/cljs-test"]
                         :compiler {:output-to "target/testable.js"
                                    :output-dir "target/js"
                                    :source-map "target/testable.js.map"
@@ -41,18 +40,18 @@
                                    :static-fns true}}]}
 
   :cljx {:builds [{:source-paths ["src/cljx"]
-                   :output-path "target/classes"
+                   :output-path "target/clj"
                    :rules :clj}
                   {:source-paths ["src/cljx"]
-                   :output-path "target/classes"
+                   :output-path "target/cljs"
                    :rules :cljs}
                   {:source-paths ["test"]
-                   :output-path "target/test-classes"
+                   :output-path "target/clj-test"
                    :rules :clj}
                   {:source-paths ["test"]
-                   :output-path "target/test-classes"
+                   :output-path "target/cljs-test"
                    :rules :cljs}]}
 
-  :aliases {"all" ["with-profile" "+dev:+1.5:+1.6:+1.7"]
+  :aliases {"all" ["with-profile" "+dev:+1.7"]
             "deploy" ["do" "clean," "deploy" "clojars"]
-            "test" ["do" "clean," "test," "cljsbuild" "test"]})
+            "test" ["do" "clean," "cljx," "test," "cljsbuild" "test"]})
