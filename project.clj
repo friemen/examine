e699961d09ccf1c044c40147ec508da7e06790f2
Falko Riemenschneider <riemenschneider@doctronic.de>
2015-10-14 16:04:03 +0200

    Use of .cljc and cljs.test

--- project.clj
+++ project.clj
@@ -5,23 +5,20 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.48" :scope "provided"]]
-  :jar-exclusions [#"\.cljx"]
+  ;:jar-exclusions [#"\.cljx"]
   :plugins [[codox "0.8.10"]
-            [org.clojars.frozenlock/cljx "0.4.6"]
+            ;[org.clojars.frozenlock/cljx "0.4.6"]
             [lein-cljsbuild "1.1.0"]
-            [com.cemerick/clojurescript.test "0.3.1"]]
+            #_[com.cemerick/clojurescript.test "0.3.1"]]
   :codox {:defaults {}
-          :sources ["src/clj" "target/classes"]
+          :sources ["src"]
           :exclude []
-          :src-uri-mapping {#"target/classes" #(str "src/cljx/" % "x")}
           :src-dir-uri "https://github.com/friemen/examine/blob/master/"
           :src-linenum-anchor-prefix "L"}
   :scm {:name "git"
         :url "https://github.com/friemen/examine"}
   :repositories [["clojars" {:url "https://clojars.org/repo"
                              :creds :gpg}]]
-  :source-paths ["src/clj"]
-  :test-paths ["target/clj" "target/clj-test"]
   :profiles {:dev {:plugins [[com.cemerick/austin "0.1.6"]]
                    :injections [(require 'cemerick.austin.repls)
                                 (defn browser-repl []
@@ -30,27 +27,14 @@
 
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
 
-  :cljsbuild {:test-commands {"unit-tests" ["phantomjs" :runner "target/testable.js"]}
-              :builds [{:source-paths ["src/clj" "target/clj" "target/cljs" "target/cljs-test"]
+  :cljsbuild {:test-commands {"unit-tests" ["phantomjs" "phantom/unit-test.js" "phantom/unit-test.html"]}
+              :builds [{:source-paths ["src" "test"]
                         :compiler {:output-to "target/testable.js"
                                    :output-dir "target/js"
                                    :source-map "target/testable.js.map"
-                                   :optimizations :advanced
+                                   :optimizations :whitespace
                                    :static-fns true}}]}
-
-  :cljx {:builds [{:source-paths ["src/cljx"]
-                   :output-path "target/clj"
-                   :rules :clj}
-                  {:source-paths ["src/cljx"]
-                   :output-path "target/cljs"
-                   :rules :cljs}
-                  {:source-paths ["test"]
-                   :output-path "target/clj-test"
-                   :rules :clj}
-                  {:source-paths ["test"]
-                   :output-path "target/cljs-test"
-                   :rules :cljs}]}
 
   :aliases {"all" ["with-profile" "+dev:+1.7"]
             "deploy" ["do" "clean," "deploy" "clojars"]
-            "test" ["do" "clean," "cljx," "test," "cljsbuild" "test"]})
+            "test" ["do" "clean," "test," "cljsbuild" "test"]})
